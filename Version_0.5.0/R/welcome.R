0be587ffa3a906eee62d1d2615bf74866fd8c73a
Xie Lab <80494699+xielab2017@users.noreply.github.com>
2022-10-20 19:04:44 +0800

    Delete Version_0.5.0 directory

--- Version_0.5.0/R/welcome.R
+++
@@ -1,8 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage("-------------------------------------------------")
-  packageStartupMessage("EMP is ready to use.")
-  packageStartupMessage("Typing for more information: help (EMP) ")
-  packageStartupMessage("Suggestions and bug-reports can be submitted at: ")
-  packageStartupMessage("https://github.com/xielab2017/EasyMicroPlot")
-  packageStartupMessage("-------------------------------------------------")
-}
